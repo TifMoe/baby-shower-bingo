1cde77312e3f44e14a70609c7d86d96179879bff
Tiffany Moeller <tmoellerart@gmail.com>
2021-07-13 08:58:19 +0100

    Add fox animations

--- frontend/src/components/FoxSquare.js
+++ frontend/src/components/FoxSquare.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { gsap, CSSPlugin } from "gsap/all";
+import { gsap, TweenLite, CSSPlugin } from "gsap/all";
 
 import { ReactComponent as Fox } from '../assets/fox.svg';
 import './FoxSquare.css'
@@ -8,11 +8,11 @@ import './FoxSquare.css'
 const plugins = [ CSSPlugin ];
 
 export default function FoxSquare() {
+    document.addEventListener("mousemove", onMouseMove);
 
     useEffect(() => {
-        // const repeat = new TimelineMax({repeat: -1})
-        // repeat.add(animateEyes())
         blink()
+        twitchEar()
     }, [])
 
     const blink = () => {
@@ -28,14 +28,43 @@ export default function FoxSquare() {
           .timeline({
             repeat: -1,
             repeatDelay: 3,
-            defaults: { duration: .1 }
+            defaults: { duration: .05 }
           })
-          .add('blink')
-          .to('#left-eye', { scaleY: 0 }, 'blink')
-          .to('#left-eye', { scaleY: 1 }, 'blink')
-          .to('#right-eye', { scaleY: 0 }, 'blink')
-          .to('#right-eye', { scaleY: 1 }, 'blink')
-      }
+          .add('close')
+          .to('#left-eye', { scaleY: 0 }, 'close')
+          .to('#right-eye', { scaleY: 0 }, 'close')
+
+          .add('open')
+          .to('#left-eye', { scaleY: 1 }, 'open')
+          .to('#right-eye', { scaleY: 1 }, 'open')
+    }
+
+    const twitchEar = () => {
+        gsap.set('#left-ear', {
+            transformOrigin: "100% 15%",
+          })
+
+        gsap
+          .timeline({
+            repeat: -1,
+            repeatDelay: 5,
+            defaults: { duration: .05 }
+          })
+          .to('#left-ear', 0.5, { rotation: -5, ease: "circ.out" })
+    }
+
+    function onMouseMove(event) {
+        var xPos = (event.clientX/window.innerWidth)-0.5,
+            yPos = (event.clientY/window.innerHeight)-0.5;
+
+        gsap.set('#face', {
+            transformOrigin: "50% 50%",
+        })
+        TweenLite.to('#face', 0.6, {rotation: -xPos * 10, ease: "circ.out"})
+        TweenLite.to('#left-pupil', 0.1, { x: xPos * 200, y: yPos * 200, ease: "circ.out" })
+        TweenLite.to('#right-pupil', 0.1, { x: xPos * 200, y: yPos * 200, ease: "circ.out" })
+        ; 
+    };
 
     return (
         <div className="svg-container">
